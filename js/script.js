7a9fbfe3361369631d9363242f78695c929bcecf
RafaelAlegranzziKlein <rafaelklein2007@gmail.com>
2025-12-11 19:28:31 -0300

    blablabla

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let valorDaConta = 100 ;
+let taxaJuros = 1;
+const taxaDecimal = taxaJuros/100;
+const taxaJurosMensal = taxaDecimal*30;
+
+const valorTotal = valorConta * taxaJurosMensal
+console.log(valorConta + " , " + taxaJurosMensal + " , " + valorTotal)
