1c8622e031a2eea51f9dd3e88ce5fb62c401e2a0
RafaelAlegranzziKlein <rafaelklein2007@gmail.com>
2025-12-11 20:18:15 -0300

    boeladelfuego

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 
-let valorConta = 65 ;
-let taxaJuros = 50;
+let valorConta = 0 ;
+let taxaJuros = 0;
 const taxaDecimal = taxaJuros/100;
 const taxaJurosMensal = taxaDecimal*30;
 
 const valorTotal = valorConta + taxaJurosMensal
 console.log(valorConta + " , " + taxaJurosMensal + " , " + valorTotal)
+
+
