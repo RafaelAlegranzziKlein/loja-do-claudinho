06e40cb2a09a6afcf1c8bcd7506a43d5027e5efc
RafaelAlegranzziKlein <rafaelklein2007@gmail.com>
2025-12-11 19:47:09 -0300

    script att

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-let valorDaConta = 100 ;
-let taxaJuros = 1;
+let valorConta = 65 ;
+let taxaJuros = 50;
 const taxaDecimal = taxaJuros/100;
 const taxaJurosMensal = taxaDecimal*30;
 
-const valorTotal = valorConta * taxaJurosMensal
+const valorTotal = valorConta + taxaJurosMensal
 console.log(valorConta + " , " + taxaJurosMensal + " , " + valorTotal)
