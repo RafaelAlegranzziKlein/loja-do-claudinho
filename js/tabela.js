c283ccd91c461810d945cf9b5de72a5e10825fe9
RafaelAlegranzziKlein <rafaelklein2007@gmail.com>
2025-12-12 22:15:59 -0300

    teabelas de clientes

--- js/tabela.js
+++ js/tabela.js
@@ -1,7 +1,7 @@
 import { db } from './firebaseConfig.js';
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { collection, addDoc, server } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
- function getInput(){
+function getInput() {
     return {
         nome: document.getElementById("nome"),
         idade: document.getElementById("idade"),
@@ -9,43 +9,68 @@ import { collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/fi
     }
 }
 
-function getValores({nome, idade, cargo}){
+function getValores({ nome, idade, cargo }) {
     return {
         nome: nome.value.trim(),
         idade: parseInt(idade.value),
         cargo: cargo.value.trim()
     }
 }
-document.getElementById("btnEnviar").addEventListener("click", async function (){
+document.getElementById("btnEnviar").addEventListener("click", async function () {
     const Inputs = getInput()
     const dados = getValores(Inputs)
 
     console.log("Dados", dados)
 
-    try{
+    try {
         const ref = await addDoc(collection(db, "funcionarios"), dados)
         console.log("ID do documento", ref.id)
         alert("Funcionario cadastrado com sucesso.")
-    } catch (e){
+    } catch (e) {
         console.log("Erro:", e)
     }
 })
 
-function getInput() {
-    return{
-        Nome: document.getElementById("Nome"),
-        CPF: document.getElementById("CPF"),
-        dividas: document.getElementById("dividas"),
-        nivel_de_fidelidade: document.getElementById("nivel_de_fidelidade"),
-    };
+
+/**
+ * @author : Rafael Alegranzzi Klein
+ *  os codigos abaixo foram feitos por minha pessoa
+ * O codigo é do cliente
+ */
+
+function getImputCliente() {
+    return {
+        cliente: document.getElementById("nomeCliente"),
+        cpf: document.getElementById("cpf"),
+        dividaDoCliente: document.getElementById("dividaCliente"),
+        taxaDeJurosDia: document.getElementById("taxaJurosCliente"),
+        vencimentoDivida: document.getElementById("dataVencimetoCliente")
+    }
 }
 
-function getValores({ Nome,CPF,dividas,nivel_de_fidelidade}){
-return{
-    Nome: Nome.value.trim(),
-    CPF: CPF.value.trim(),
-    dividas: dividas.value.trim(),
-    nivel_de_fidelidade: nivel_de_fidelidade.value.trim(),
-};
+function getValoresCliente({ cliente, cpf, dividaDoCliente, taxaDeJurosDia, vencimentoDivida }) {
+    return {
+        valueCliente: cliente.value.trim(),
+        valueCPF: parseFloat(cpf.value.trim()),
+        valueDividaCliente: Math.round(parseFloat(dividaDoCliente.value) * 10) / 100,
+        valueTxaJuros: taxaDeJurosDia / 100,
+        valueVencimentoDivida: vencimentoDivida.trim()
 
+    }
 }
+
+
+document.getElementById("btnEnviarCliente").addEventListener("click", async function () {
+    const Inputs = getImputCliente()
+    const dados = getValoresCliente(Inputs)
+
+    console.log("Dados", dados)
+
+    try {
+        const ref = await addDoc(collection(db, "Clientes"), dados)
+        console.log("ID do documento", ref.id)
+        alert("Cliente cadastrado com sucesso.")
+    } catch (e) {
+        console.log("Erro:", e)
+    }
+})
